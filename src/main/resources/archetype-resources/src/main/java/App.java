c91baa777284717936422b0a8baff18484b22231
egydGIT <egyd.eszter@gmail.com>
2024-02-19 18:43:13 +0100

    First commit to the new branch from TTA983's PC!

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $TTA983_example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
